9f6a0c56a7e031668ac03e91120a0fb235d5dcbe
Esther Kang <estherkang14@gmail.com>
2020-05-19 19:56:20 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,9 +8,8 @@ while row_index < src.length do
   element_index = 0 
   
   while element_index < src[row_index].length do 
-    
     if src[row_index][element_index].even? 
-      puts src[row_index][element_index] 
+      p src[row_index][element_index] 
       element_index += 1 
     else 
       element_index += 1 
